74b3a689cdd79d1ed8193f1e1e22560e2e283a5a
Nicotinii <nml.lefevre@gmail.com>
2022-11-04 17:52:11 +0100

    OPP_CHALL2

--- index.php
+++ index.php
@@ -2,19 +2,34 @@
 
 require_once('Bicycle.php');
 require_once('Car.php');
+require_once('Truck.php');
 
 $bike = new Bicycle('red', 1);
-$car1 = new Car('pink', 4, 'electric');
+$c1 = new Car('pink', 4, 'electric');
+$amazon = new Truck('purple', 2, 'electric', 40);
 
-echo $bike->forward();
-echo $bike->brake() . "<br>";
+$amazon->setLoading(10);
+echo $amazon->fillingTruck();
 
-echo $car1->start();
-echo $car1->forward();
-echo $car1->brake() . "<br>";
+echo $amazon->start();
+$amazon->setCurrentSpeed(80);
+echo $amazon->forward();
+echo $amazon->brake();
 
 
-echo $car1->start();
-echo $car1->forward();
-$car1->setCurrentSpeed(200);
-echo $car1->brake();
+$amazon->setLoading(40);
+echo $amazon->fillingTruck();
+
+echo $amazon->start();
+$amazon->setCurrentSpeed(80);
+echo $amazon->forward();
+echo $amazon->brake();
+
+
+var_dump($amazon);
+
+
+echo '<br>' . $c1->start();
+$c1->setCurrentSpeed(200);
+echo $c1->forward();
+echo $c1->brake();
